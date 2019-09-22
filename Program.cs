caa08aa1a4cb90c2fffd4c702d0a1c029d50abcb
Chicago-Test <54727971+Chicago-Test@users.noreply.github.com>
2019-09-22 18:12:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1 {
+    class Program {
+        static void Main (string[] args) {
+            long i = 0;
+            DirectoryInfo dirInfo = new DirectoryInfo (@"E:\temp");
+
+            //foreach (var f in Directory.EnumerateFiles(@"E:\temp", "*", SearchOption.AllDirectories)) {
+            foreach (var f in Directory.EnumerateDirectories(@"E:\", "*", SearchOption.AllDirectories)) {
+                //Console.WriteLine ("{0}", f.ToString());
+                i++;
+            }
+            Console.WriteLine ("{0}", i);
+        }
+    }
+}
